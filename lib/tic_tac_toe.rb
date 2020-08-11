6f7976e23ebc264bfb9e7e3f851f4d203e81ddd3
Vietkhoi Nguyen <khoinguyenkc@gmail.com>
2020-08-11 20:08:49 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -16,96 +16,6 @@ WIN_COMBINATIONS = [
   [0,4,8],
   [2,4,6]
 ]
-
-def won?(board)
-  WIN_COMBINATIONS.each do |combo|
-    # if same value in this row/column/diag and not empty empty empty
-    if ( (board[combo[0]] == board[combo[1]]) &&  (board[combo[1]] == board[combo[2]] ) && position_taken?(board, combo[0]) )
-      return combo
-    #critical:
-    #do not put an else return nil here. or it will end the entire operation. only return nill if it's looped through EVERYTHING and still found nothing
-    end
-  end
-
-  return nil
-end
-
-#hi = won?(myboard)
-#puts hi
-
-def full?(board)
-  board.none? do |item|
-    item == " " || item   == ""
-  end
-end
-
-def draw?(board)
-  if full?(board) && won?(board) == nil
-    return true
-  elsif !full?(board) && won?(board) == nil
-    return false
-  else full?(board) || !full?(board)
-    #ridiculous but it looks like certain boards fail the won and thus the full method was never called and that fails a tests
-    #weirdly this still says full was not called with that particular board they used for testing
-    return false
-  end
-end
-
-drawboard = ["X", "O", "X", "O", "X", "X", "O", "X", "O"]
-incomplete_board = ["X", " ", " ", " ", " ", " ", " ", " ", " "]
-wonboard = ["X", "X", "X", " ", " ", " ", " ", " ", " "]
-puts draw?(incomplete_board) #=> false
-puts draw?(drawboard) #true
-puts draw?(wonboard) #false
-
-
-def over?(board)
-  if draw?(board) || won?(board) || full?(board)
-    return true
-  else
-    return false
-  end
-end
-
-def winner(board)
-  #ex: if winningcombo is [2,4,6], board[winningcombo[0]] = board[2] = either X or O
-  winningcombo = won?(board)
-  if winningcombo == nil
-    return nil
-  else
-    return board[winningcombo[0]]
-  end
-end
-
-
-#----------------------
-#----------------------
-#----------------------
-#CURRENT player
-def turn_count(board)
-  turns = 0
-
-  board.each do | item |
-    if item == "X" || item == "O"
-      turns +=1
-    end
-  end
-
-  return turns
-end
-
-def current_player(board)
-  turns = turn_count(board)
-
-  if (turns % 2 == 0)
-    return "X"
-  else
-    return "O"
-  end
-
-end
-
-
 #----------------------
 #----------------------
 #----------------------
@@ -177,6 +87,95 @@ def play(board)
 end
 
 
+
+
 #----------------------
 #----------------------
 #----------------------
+#CURRENT player
+def turn_count(board)
+  turns = 0
+
+  board.each do | item |
+    if item == "X" || item == "O"
+      turns +=1
+    end
+  end
+
+  return turns
+end
+
+def current_player(board)
+  turns = turn_count(board)
+
+  if (turns % 2 == 0)
+    return "X"
+  else
+    return "O"
+  end
+
+end
+
+#----------------------
+#----------------------
+#----------------------
+
+def won?(board)
+  WIN_COMBINATIONS.each do |combo|
+    # if same value in this row/column/diag and not empty empty empty
+    if ( (board[combo[0]] == board[combo[1]]) &&  (board[combo[1]] == board[combo[2]] ) && position_taken?(board, combo[0]) )
+      return combo
+    #critical:
+    #do not put an else return nil here. or it will end the entire operation. only return nill if it's looped through EVERYTHING and still found nothing
+    end
+  end
+
+  return nil
+end
+
+#hi = won?(myboard)
+#puts hi
+
+def full?(board)
+  board.none? do |item|
+    item == " " || item   == ""
+  end
+end
+
+def draw?(board)
+  if full?(board) && won?(board) == nil
+    return true
+  elsif !full?(board) && won?(board) == nil
+    return false
+  else full?(board) || !full?(board)
+    #ridiculous but it looks like certain boards fail the won and thus the full method was never called and that fails a tests
+    #weirdly this still says full was not called with that particular board they used for testing
+    return false
+  end
+end
+
+drawboard = ["X", "O", "X", "O", "X", "X", "O", "X", "O"]
+incomplete_board = ["X", " ", " ", " ", " ", " ", " ", " ", " "]
+wonboard = ["X", "X", "X", " ", " ", " ", " ", " ", " "]
+puts draw?(incomplete_board) #=> false
+puts draw?(drawboard) #true
+puts draw?(wonboard) #false
+
+
+def over?(board)
+  if draw?(board) || won?(board) || full?(board)
+    return true
+  else
+    return false
+  end
+end
+
+def winner(board)
+  #ex: if winningcombo is [2,4,6], board[winningcombo[0]] = board[2] = either X or O
+  winningcombo = won?(board)
+  if winningcombo == nil
+    return nil
+  else
+    return board[winningcombo[0]]
+  end
+end
